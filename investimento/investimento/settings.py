fbed4b550d1422bce551facb8d716acd2585de4b
David <machadodavid32@gmail.com>
2023-11-13 19:11:58 -0300

    atualizando arquivos para o render

--- investimento/investimento/settings.py
+++ investimento/investimento/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j%cbi14(m@4q@-!jx@!$izsz#ps^s-!$nw90&_#2d6wo(9h)pg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # vou conseguir me conectar em qualquer local no sistema web
 
 
 # Application definition
